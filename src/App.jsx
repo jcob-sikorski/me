63a229c00195583c62d70c44ef8859f837f7a819
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2026-02-07 19:11:13 +0100

    Switch to BrowserRouter and add 404.html for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 // src/App.jsx
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import BlogList from "./BlogList";
 import BlogPost from "./BlogPost";
 
 export default function App() {
   return (
-    // We use HashRouter for easy compatibility with GitHub Pages
-    <HashRouter>
+    <BrowserRouter basename="/me">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/writing" element={<BlogList />} />
         <Route path="/writing/:slug" element={<BlogPost />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
